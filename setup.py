aa24b9bbcab7e1a8a68244e84775b2b36ca84189
edwardmjackson <31844712+edwardmjackson@users.noreply.github.com>
2020-02-07 22:25:06 +0000

    install_requires

--- setup.py
+++ setup.py
@@ -14,20 +14,7 @@
 
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
-from pathlib import Path
-import json
-import urllib.request
-from functools import lru_cache
-
-
-@lru_cache(maxsize=50)
-def _get_github_sha(github_install_url: str):
-    """From the github_install_url get the hash of the latest commit"""
-    repository = Path(github_install_url).stem.split('#egg', 1)[0]
-    organisation = Path(github_install_url).parent.stem
-    with urllib.request.urlopen(f'https://api.github.com/repos/{organisation}/{repository}/commits/master') as response:
-        return json.loads(response.read())['sha']
-
+from pathlib import Path 
 
 setup(
     name=NAME,
@@ -47,25 +34,10 @@ setup(
         'sanic==0.6.0',
         'numexpr==2.6.5.dev0',
         'cape.client==0.2.0',
-        'cape_userdb==' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-userdb#egg=cape_userdb'),
-        'cape_api_helpers==' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers'),
-        'cape_responder==' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-responder#egg=cape_responder'),
-        'cape_document_manager==' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-document-manager#egg=cape_document_manager'),
-    ],
-    dependency_links=[
-        'git+https://github.com/pydata/numexpr@cfeae8ae246e95f23613e8b587746ed788b81f35#egg=numexpr-2.6.5.dev0',
-        'git+https://github.com/bloomsburyai/cape-userdb#egg=cape_userdb-' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-userdb#egg=cape_userdb'),
-        'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers-' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-api-helpers#egg=cape_api_helpers'),
-        'git+https://github.com/bloomsburyai/cape-responder#egg=cape_responder-' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-responder#egg=cape_responder'),
-        'git+https://github.com/bloomsburyai/cape-document-manager#egg=cape_document_manager-' + _get_github_sha(
-            'git+https://github.com/bloomsburyai/cape-document-manager#egg=cape_document_manager'),
+        'cape_userdb @ git+https://github.com/bloomsburyai/cape-userdb',
+        'cape_api_helpers @ git+https://github.com/bloomsburyai/cape-api-helpers',
+        'cape_responder @ git+https://github.com/bloomsburyai/cape-responder',
+        'cape_document_manager @ git+https://github.com/bloomsburyai/cape-document-manager',
     ],
     package_data={
         '': ['*.*'],
